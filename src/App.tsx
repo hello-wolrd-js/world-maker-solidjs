c81ffca03edc0d58eb573a1226bc27e250b20155
Lin <2295141009@stu.hqu.edu.cn>
2024-04-16 18:48:00 +0800

    feat: 计数器demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import type { Component } from "solid-js";
-
-import styles from "./App.module.css";
+import { createSignal, type Component } from "solid-js";
 
 const App: Component = () => {
-    return <div class={styles.test}>123</div>;
+    const [count, setCount] = createSignal(0);
+    const increment = () => {
+        setCount(count() + 1);
+    };
+    const decrease = () => {
+        setCount(count() - 1);
+    };
+    return (
+        <div class="p-10">
+            <div>count:{count()}</div>
+            <button class="btn" onClick={increment}>
+                +
+            </button>
+            <button class="btn ml-1" onClick={decrease}>
+                -
+            </button>
+        </div>
+    );
 };
 
 export default App;
