e940060b1207c6090f654c1431f7e0f01ef744ba
Tyee <flin00806@gmail.com>
2024-05-06 17:53:32 +0800

    feat: 增加路由测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { createSignal, type Component } from "solid-js";
+import { useNavigate } from "@solidjs/router";
+import { JSXElement, createSignal, type Component } from "solid-js";
 
-const App: Component = () => {
+const App: Component<{ children?: JSXElement }> = (props) => {
     const [count, setCount] = createSignal(0);
     const increment = () => {
         setCount(count() + 1);
@@ -8,15 +9,32 @@ const App: Component = () => {
     const decrease = () => {
         setCount(count() - 1);
     };
+    const navigator = useNavigate();
     return (
-        <div class="p-10">
-            <div>count:{count()}</div>
-            <button class="btn" onClick={increment}>
-                +
-            </button>
-            <button class="btn ml-1" onClick={decrease}>
-                -
-            </button>
+        <div class="flex-col p-10">
+            <div>
+                <h1>signal test</h1>
+                <div>count:{count()}</div>
+                <button class="btn" onClick={increment}>
+                    +
+                </button>
+                <button class="btn ml-1" onClick={decrease}>
+                    -
+                </button>
+            </div>
+            <div class="mt-5">
+                <h1>route test</h1>
+                <div class="flex gap-2 mt-2">
+                    <button class="btn" onClick={() => navigator("/kancy")}>
+                        ToKancy
+                    </button>
+                    <button class="btn" onClick={() => navigator("/tyee")}>
+                        ToTyee
+                    </button>
+                    <button class="btn btn-warning" onClick={()=>navigator('/')}>Reset</button>
+                </div>
+            </div>
+            <div class="mt-5">{props.children}</div>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,33 @@
 import { render } from "solid-js/web";
 import App from "./App";
 import "./index.css";
+import { Route, Router } from "@solidjs/router";
 
 const root = document.getElementById("root");
 
+const Kancy = () => {
+    return <div>this is kancy!</div>;
+};
+
+const Tyee = () => {
+    return <div>this is tyee!</div>;
+};
+
+const Main = () => {
+    return (
+        <Router root={App}>
+            <Route path="/kancy" component={Kancy}></Route>
+            <Route path="/tyee" component={Tyee}></Route>
+        </Router>
+    );
+};
+
 //开发环境下执行render
 if (import.meta.env.DEV && root instanceof HTMLElement) {
-    render(() => <App />, root!);
+    render(Main, root!);
 }
 
 //默认导出
 export default (el: HTMLElement) => {
-    render(() => <App />, el);
+    render(Main, el);
 };
